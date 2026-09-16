0695bbf8378a0bf90b0456b311f5931372641808
meetclove <2648892871@qq.com>
2026-09-17 01:12:30 +0800

    Add files via upload

--- patches/patches.sh
+++ patches/patches.sh
@@ -1,76 +1,115 @@
 #!/bin/bash
-# Patches author: weishu <twsxtd@gmail.com>
-# Shell authon: xiaoleGun <1592501605@qq.com>
-#               bdqllW <bdqllT@gmail.com>
-# Tested kernel versions: 5.4, 4.19, 4.14, 4.9
-# 20240123
+# ReSukiSU manual hook patches for non-GKI kernel (gauguin 4.19)
+# Required hooks: fs/stat.c, fs/exec.c, fs/open.c, kernel/reboot.c
+# Config macro: CONFIG_KSU_MANUAL_HOOK
 
 patch_files=(
+    fs/stat.c
     fs/exec.c
     fs/open.c
-    fs/read_write.c
-    fs/stat.c
-    drivers/input/input.c
+    kernel/reboot.c
 )
 
 for i in "${patch_files[@]}"; do
 
-    if grep -q "ksu" "$i"; then
-        echo "Warning: $i contains KernelSU"
+    if grep -q "CONFIG_KSU_MANUAL_HOOK" "$i"; then
+        echo "Warning: $i already contains ReSukiSU manual hook"
         continue
     fi
 
     case $i in
 
-    # fs/ changes
-    ## exec.c
-    fs/exec.c)
-        sed -i '/static int do_execveat_common/i\#ifdef CONFIG_KSU\nextern bool ksu_execveat_hook __read_mostly;\nextern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,\n			void *envp, int *flags);\nextern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,\n				 void *argv, void *envp, int *flags);\n#endif' fs/exec.c
-        if grep -q "return __do_execve_file(fd, filename, argv, envp, flags, NULL);" fs/exec.c; then
-            sed -i '/return __do_execve_file(fd, filename, argv, envp, flags, NULL);/i\	#ifdef CONFIG_KSU\n	if (unlikely(ksu_execveat_hook))\n		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);\n	else\n		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);\n	#endif' fs/exec.c
-        else
-            sed -i '/if (IS_ERR(filename))/i\	#ifdef CONFIG_KSU\n	if (unlikely(ksu_execveat_hook))\n		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);\n	else\n		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);\n	#endif' fs/exec.c
-        fi
-        ;;
-
-    ## open.c
-    fs/open.c)
-        if grep -q "long do_faccessat(int dfd, const char __user \*filename, int mode)" fs/open.c; then
-            sed -i '/long do_faccessat(int dfd, const char __user \*filename, int mode)/i\#ifdef CONFIG_KSU\nextern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,\n			 int *flags);\n#endif' fs/open.c
-        else
-            sed -i '/SYSCALL_DEFINE3(faccessat, int, dfd, const char __user \*, filename, int, mode)/i\#ifdef CONFIG_KSU\nextern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,\n			 int *flags);\n#endif' fs/open.c
-        fi
-        sed -i '/if (mode & ~S_IRWXO)/i\	#ifdef CONFIG_KSU\n	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);\n	#endif\n' fs/open.c
-        ;;
-
-    ## read_write.c
-    fs/read_write.c)
-        sed -i '/ssize_t vfs_read(struct file/i\#ifdef CONFIG_KSU\nextern bool ksu_vfs_read_hook __read_mostly;\nextern int ksu_handle_vfs_read(struct file **file_ptr, char __user **buf_ptr,\n		size_t *count_ptr, loff_t **pos);\n#endif' fs/read_write.c
-        sed -i '/ssize_t vfs_read(struct file/,/ssize_t ret;/{/ssize_t ret;/a\
-        #ifdef CONFIG_KSU\
-        if (unlikely(ksu_vfs_read_hook))\
-            ksu_handle_vfs_read(&file, &buf, &count, &pos);\
-        #endif
-        }' fs/read_write.c
-        ;;
-
-    ## stat.c
+    # fs/stat.c - stat/fstat/fstatat hooks
     fs/stat.c)
-        if grep -q "int vfs_statx(int dfd, const char __user \*filename, int flags," fs/stat.c; then
-            sed -i '/int vfs_statx(int dfd, const char __user \*filename, int flags,/i\#ifdef CONFIG_KSU\nextern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);\n#endif' fs/stat.c
-            sed -i '/unsigned int lookup_flags = LOOKUP_FOLLOW | LOOKUP_AUTOMOUNT;/a\\n	#ifdef CONFIG_KSU\n	ksu_handle_stat(&dfd, &filename, &flags);\n	#endif' fs/stat.c
-        else
-            sed -i '/int vfs_fstatat(int dfd, const char __user \*filename, struct kstat \*stat,/i\#ifdef CONFIG_KSU\nextern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);\n#endif\n' fs/stat.c
-            sed -i '/if ((flag & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |/i\	#ifdef CONFIG_KSU\n	ksu_handle_stat(&dfd, &filename, &flag);\n	#endif\n' fs/stat.c
+        if grep -q "SYSCALL_DEFINE4(newfstatat" "$i"; then
+            sed -i '/SYSCALL_DEFINE4(newfstatat/i\
+#ifdef CONFIG_KSU_MANUAL_HOOK\
+__attribute__((hot))\
+extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);\
+extern void ksu_handle_newfstat_ret(unsigned int *fd, struct stat __user **statbuf_ptr);\
+#if defined(__ARCH_WANT_STAT64) || defined(__ARCH_WANT_COMPAT_STAT64)\
+extern void ksu_handle_fstat64_ret(unsigned long *fd, struct stat64 __user **statbuf_ptr);\
+#endif\
+#endif' "$i"
+            sed -i '/int error;/a\
+#ifdef CONFIG_KSU_MANUAL_HOOK\
+\tksu_handle_stat(&dfd, &filename, &flag);\
+#endif' "$i"
+        fi
+        if grep -q "SYSCALL_DEFINE2(newfstat" "$i"; then
+            sed -i '/SYSCALL_DEFINE2(newfstat/,/return error;/{/return error;/i\
+#ifdef CONFIG_KSU_MANUAL_HOOK\
+\tksu_handle_newfstat_ret(&fd, &statbuf);\
+#endif
+            }' "$i"
+        fi
+        if grep -q "SYSCALL_DEFINE2(fstat64" "$i"; then
+            sed -i '/SYSCALL_DEFINE2(fstat64/,/return error;/{/return error;/i\
+#ifdef CONFIG_KSU_MANUAL_HOOK // for 32-bit\
+\tksu_handle_fstat64_ret(&fd, &statbuf);\
+#endif
+            }' "$i"
+        fi
+        if grep -q "SYSCALL_DEFINE4(fstatat64" "$i"; then
+            sed -i '/SYSCALL_DEFINE4(fstatat64/i\
+#ifdef CONFIG_KSU_MANUAL_HOOK\
+__attribute__((hot))\
+extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);\
+#endif' "$i"
+            sed -i '/SYSCALL_DEFINE4(fstatat64/,/int error;/{/int error;/a\
+#ifdef CONFIG_KSU_MANUAL_HOOK // 32-bit su\
+\tksu_handle_stat(&dfd, &filename, &flag);\
+#endif
+            }' "$i"
         fi
         ;;
 
-    # drivers/input changes
-    ## input.c
-    drivers/input/input.c)
-        sed -i '/static void input_handle_event/i\#ifdef CONFIG_KSU\nextern bool ksu_input_hook __read_mostly;\nextern int ksu_handle_input_handle_event(unsigned int *type, unsigned int *code, int *value);\n#endif\n' drivers/input/input.c
-        sed -i '/int disposition = input_get_disposition(dev, type, code, &value);/a\	#ifdef CONFIG_KSU\n	if (unlikely(ksu_input_hook))\n		ksu_handle_input_handle_event(&type, &code, &value);\n	#endif' drivers/input/input.c
+    # fs/exec.c - execve hooks
+    fs/exec.c)
+        sed -i '/int do_execve(struct filename \*filename/i\
+#ifdef CONFIG_KSU_MANUAL_HOOK\
+__attribute__((hot))\
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr,\
+\t\t\tvoid *argv, void *envp, int *flags);\
+#endif' "$i"
+        sed -i '/int do_execve(struct filename \*filename/,/return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);/{/struct user_arg_ptr envp = { .ptr.native = __envp };/a\
+#ifdef CONFIG_KSU_MANUAL_HOOK\
+\tksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);\
+#endif
+        }' "$i"
+        sed -i '/static int compat_do_execve(struct filename \*filename/,/return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);/{/\.ptr.compat = __envp,/a\
+#ifdef CONFIG_KSU_MANUAL_HOOK // 32-bit ksud and 32-on-64 support\
+\tksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);\
+#endif
+        }' "$i"
         ;;
-    esac
 
+    # fs/open.c - faccessat hook
+    fs/open.c)
+        sed -i '/SYSCALL_DEFINE3(faccessat/i\
+#ifdef CONFIG_KSU_MANUAL_HOOK\
+__attribute__((hot))\
+extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode, int *flags);\
+#endif' "$i"
+        sed -i '/SYSCALL_DEFINE3(faccessat/,/return do_faccessat/{/return do_faccessat/i\
+#ifdef CONFIG_KSU_MANUAL_HOOK\
+\tksu_handle_faccessat(&dfd, &filename, &mode, NULL);\
+#endif
+        }' "$i"
+        ;;
+
+    # kernel/reboot.c - sys_reboot hook
+    kernel/reboot.c)
+        sed -i '/SYSCALL_DEFINE4(reboot/i\
+#ifdef CONFIG_KSU_MANUAL_HOOK\
+extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);\
+#endif' "$i"
+        sed -i '/SYSCALL_DEFINE4(reboot/,/int ret = 0;/{/int ret = 0;/a\
+#ifdef CONFIG_KSU_MANUAL_HOOK\
+\tksu_handle_sys_reboot(magic1, magic2, cmd, &arg);\
+#endif
+        }' "$i"
+        ;;
+
+    esac
 done
